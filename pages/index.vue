fab0b3081ba8c95c0bf3c6d3ad3323833de4ae4d
simoman83 <58381684+simoman83@users.noreply.github.com>
2021-06-25 22:17:11 +0200

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -6,7 +6,7 @@
         class="button is-primary"
         @click="$eventBus.$emit('modal-triggered', 'newsletter-modal')"
       >
-        simoabkari
+        simoabkariahhh
       </button>
     </site-hero>
     <main-section theme="one-column">
